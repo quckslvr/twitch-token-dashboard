c5fe3b3065eedc9289eec7b5901a6c0e494e3ff9
officialmxm <dubb3rs1ck@icloud.com>
2021-10-28 01:45:45 -0400

    fix commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
-import Clock from './components/Clock';
-import Menu from './components/Menu';
-import profile from './assets/img/Profile.png'
 
 function App() {
   return (
-      <div className="App">
-        <Menu className="App-Menu"/>
-        <Clock />
-        <img alt="profile" src={ profile }/>
+      <div className='App'>
+        <Navbar />
       </div>
   );
 }
